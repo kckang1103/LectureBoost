40c2e9b4f0a08ad8a363c188178016be94494c01
Samuel Tarr <54817476+S-Tarr@users.noreply.github.com>
2023-04-02 19:21:27 -0400

    cleanup

--- backend/find_silence.sh
+++ backend/find_silence.sh
@@ -4,7 +4,4 @@ IN=$1
 THRESH=$2
 DURATION=$3
 
-echo $1
-echo $2
-
 ffmpeg -hide_banner -vn -i $IN -af "silencedetect=n=${THRESH}dB:d=${DURATION}" -f null - 2>&1 | grep "silence_end" | awk '{print $5 " " $8}' > uploads/silence.txt
