553dccbf58f6d606be3dbca75eb408be89eaaa07
Kichul Kang <kckang1103@gmail.com>
2023-04-04 00:22:05 -0400

    fixed back button

--- frontend/my-app/src/components/CheckBoxes.js
+++ frontend/my-app/src/components/CheckBoxes.js
@@ -55,7 +55,7 @@ export default function CheckBoxes(props) {
       if (email.localeCompare("") === 0) {
         emailToSend = "fake"
       }
-      const { data } = await axios.post(`http://127.0.0.1:8080/file/${whitespace}/${whitespace_val}/${subtitles}/${transcribe}/${slides}/${send_email}/${emailToSend}`, formData, {
+      const { data } = await axios.post(`http://localhost:8080/file/${whitespace}/${whitespace_val}/${subtitles}/${transcribe}/${slides}/${send_email}/${emailToSend}`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         }
--- frontend/my-app/src/components/Logo.js
+++ frontend/my-app/src/components/Logo.js
@@ -6,7 +6,7 @@ import logo from "../assets/logo.png";
 export default function Logo() {
   const navigate = useNavigate();
   return (<center><img onClick={() => {
-    window.location.reload();
     navigate("/");
+    window.location.reload();
   }} src={logo} alt="logo" height="100%" width="500" /></center>);
 }
